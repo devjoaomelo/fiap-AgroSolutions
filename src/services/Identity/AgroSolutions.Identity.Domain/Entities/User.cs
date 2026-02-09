47b52aeca58adb2e62431761b3914ea6fbfc7269
devjoaomelo <devjoaomelo@gmail.com>
2026-02-09 14:13:51 -0300

    feat(identity): Adicionado classe e interface do repositório para classe User.

---
+++ src/services/Identity/AgroSolutions.Identity.Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿namespace AgroSolutions.Identity.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string Name { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private User() { }
+
+    public User(string email, string passwordHash, string name)
+    {
+        Id = Guid.NewGuid();
+        Email = email;
+        PasswordHash = passwordHash;
+        Name = name;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdatePassword(string newPasswordHash)
+    {
+        PasswordHash = newPasswordHash;
+    }
+}
---
+++ src/services/Identity/AgroSolutions.Identity.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroSolutions.Identity.Domain.Interfaces
+{
+    internal interface IUserRepository
+    {
+    }
+}
