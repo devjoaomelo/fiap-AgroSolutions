2fd930be1ba35338da8ddc3ba9937473faacde93
devjoaomelo <devjoaomelo@gmail.com>
2026-02-09 17:28:29 -0300

    feat(identity): Adicionando o DbContext factory para EF Migrations

---
+++ src/services/Identity/AgroSolutions.Identity.Infrastructure/Data/IdentityDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AgroSolutions.Identity.Infrastructure.Data;
+
+public class IdentityDbContextFactory : IDesignTimeDbContextFactory<IdentityDbContext>
+{
+    public IdentityDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<IdentityDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=identity_db;Username=identity_user;Password=identity_pass");
+
+        return new IdentityDbContext(optionsBuilder.Options);
+    }
+}
