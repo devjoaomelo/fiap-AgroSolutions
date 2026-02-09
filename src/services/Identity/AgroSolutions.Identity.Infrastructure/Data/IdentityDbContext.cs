7fd12a9453439f7d0dcabea7320320aa78284256
devjoaomelo <devjoaomelo@gmail.com>
2026-02-09 15:19:54 -0300

    feat(identity) Implementação do UserRepository e IdentityDbContext com EF Core

---
+++ src/services/Identity/AgroSolutions.Identity.Infrastructure/Data/IdentityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AgroSolutions.Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgroSolutions.Identity.Infrastructure.Data;
+
+public class IdentityDbContext : DbContext
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base()
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(e =>
+        {
+            e.HasKey(e => e.Id);
+            e.HasIndex(e => e.Email).IsUnique();
+            e.Property(e => e.Email).IsRequired().HasMaxLength(255);
+            e.Property(e => e.Email).IsRequired().HasMaxLength(200);
+            e.Property(e => e.PasswordHash).IsRequired();
+        });
+    }
+}
---
+++ src/services/Identity/AgroSolutions.Identity.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using AgroSolutions.Identity.Domain.Entities;
+using AgroSolutions.Identity.Domain.Interfaces;
+using AgroSolutions.Identity.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgroSolutions.Identity.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly IdentityDbContext _context;
+
+    public UserRepository(IdentityDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<User?> GetByIdAsync (Guid id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> EmailExistsAsync(string email)
+    {
+        return await _context.Users
+            .AnyAsync(u => u.Email == email);
+    }
+}
