cc77fc0524f3b6e6f1ed9949b3eec1b6a9d1ca27
devjoaomelo <devjoaomelo@gmail.com>
2026-02-14 22:15:22 -0300

    feat(ingestion) Estrutura inicial. Adicionado a entidade de SensorData e a interface do repositorio

---
+++ src/services/Ingestion/AgroSolutions.Ingestion.Domain/Entities/SensorData.cs
@@ -0,0 +1,25 @@
+﻿namespace AgroSolutions.Ingestion.Domain.Entities;
+
+public class SensorData
+{
+    public Guid Id { get; private set; }
+    public Guid FieldId { get; private set; }
+    public double SoilMoisture { get; private set; }
+    public double Temperature { get; private set; }
+    public double Precipitation { get; private set; }
+    public DateTime Timestamp { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private SensorData() { }
+
+    public SensorData(Guid fieldId, double soilMoisture, double temperature, double precipitation, DateTime timestamp)
+    {
+        Id = Guid.NewGuid();
+        FieldId = fieldId;
+        SoilMoisture = soilMoisture;
+        Temperature = temperature;
+        Precipitation = precipitation;
+        Timestamp = timestamp;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
