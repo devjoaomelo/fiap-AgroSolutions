6d69972d8ca10283b3a6967a6d0dde884e90f311
devjoaomelo <devjoaomelo@gmail.com>
2026-02-14 23:04:58 -0300

    feat(ingestion) Adiciona migrations

---
+++ src/services/Ingestion/AgroSolutions.Ingestion.Infrastructure/Migrations/20260215020026_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroSolutions.Ingestion.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SensorData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FieldId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SoilMoisture = table.Column<double>(type: "double precision", nullable: false),
+                    Temperature = table.Column<double>(type: "double precision", nullable: false),
+                    Precipitation = table.Column<double>(type: "double precision", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorData", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorData_FieldId",
+                table: "SensorData",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorData_Timestamp",
+                table: "SensorData",
+                column: "Timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SensorData");
+        }
+    }
+}
